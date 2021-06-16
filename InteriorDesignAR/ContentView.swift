9c68e11d1913eaa7cb87e7058182c30aa3005667
Marc <marc.daniel.jacques@gmail.com>
2021-06-16 15:02:01 -0400

    feat: add ControlView to ContentView
    add ControlView to manage the "MostRecentlyPlaced", "Browse", and "Settings" buttons

--- InteriorDesignAR/ContentView.swift
+++ InteriorDesignAR/ContentView.swift
@@ -10,7 +10,14 @@ import RealityKit
 
 struct ContentView : View {
     var body: some View {
-        return ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack(alignment: .bottom) {
+            
+            ARViewContainer()
+            
+            ControlView()
+            
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
