3820ca9750378cdc9654c9f69016a11b3591536f
Marc <marcjacques@me.com>
2021-07-12 20:50:38 -0400

    feat: add PlacementSettings
    add ObservableObject that publishes the selectedModel
    add ObservableObject that publishes the confirmedModel

---
+++ InteriorDesignAR/PlacementSettings.swift
@@ -0,0 +1,30 @@
+//
+//  PlacementSettings.swift
+//  InteriorDesignAR
+//
+//  Created by Marc Jacques on 7/12/21.
+//
+
+import SwiftUI
+import RealityKit
+import Combine
+
+class PlacementSettings: ObservableObject {
+    // When the user selects a model in BrowseView, this property is set.
+    @Published var selectedModel: Model? {
+        willSet(newValue) {
+            print("Setting selectedModel to \(String(describing: newValue?.name))")
+        }
+    }
+    // When the user taps confirm in PlacementView, the value of selectedModel is assigned to confirmedModel
+    @Published var confirmedModel: Model? {
+        willSet(newValue) {
+            guard let model = newValue else {
+                print("Clearing confirmedModel")
+                return
+            }
+            
+            print("Setting confirmedModel to \(model.name)")
+        }
+    }
+}
