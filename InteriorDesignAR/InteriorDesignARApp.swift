0fff44e2351cb7ba3ff6288922e6a803cbcd5f1c
Marc <marcjacques@me.com>
2021-07-14 17:35:24 -0400

    fix: add launch screen file to info.plist
    replace AppDelegate with "InteriorDesignARApp"
    modify info.plist to recognize the "InteriorDesignARApp" as launch screen configuration

---
+++ InteriorDesignAR/InteriorDesignARApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteriorDesignAR.swift
+//  InteriorDesignAR
+//
+//  Created by Marc Jacques on 7/14/21.
+//
+import SwiftUI
+
+@main
+struct InteriorDesignARApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
